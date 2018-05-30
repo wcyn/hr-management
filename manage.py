8ba0cea7a751a496aa9aada620016966de39f149
wcyn <wasonga.cynthia@gmail.com>
2018-05-30 16:03:30 +0300

    Create Alembic migration files

---
+++ manage.py
@@ -0,0 +1,16 @@
+import os
+
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+from app import create_app, db
+from app.models.tasks import Task
+
+app = create_app(os.getenv('APP_ENV'))
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+if __name__ == "__main__":
+    manager.run()
+
